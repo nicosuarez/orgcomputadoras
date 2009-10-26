1bd374e06b573b649f2670c2f658d34b35389b4d
nicolas.suarez@gmail.com <nicolas.suarez@gmail.com@9c04f0e4-a400-11de-9a17-712dd7b31d32>
2009-10-26 00:58:00 +0000

    -Ya emite la salida por stdout o a un archivo de salida segun los parametros del programa. -Ahora los errores los emite por stderr

--- trunk/TP1/src/tp1.c
+++ trunk/TP1/src/tp1.c
@@ -6,51 +6,18 @@
 
 #define MAX 256
 
-//parametros pedidos en el tp son distintos
-void ordenar(int *array,int desde,int hasta)
-{
-	int i,d,aux; // i realiza la búsqueda de izquierda a derecha
-	// y j realiza la búsqueda de derecha a izquierda.
-	if(desde>=hasta)
-	return;
-
-	for(i=desde+1,d=hasta;;) // Valores iniciales de la búsqueda.
-	{
-		for(;i<=hasta && array[i]<=array[desde];i++); // Primera búsqueda
-			for(;d>=0 && array[d]>=array[desde];d--); // segunda búsqueda
-				if(i<d) // si no se han cruzado:
-				{
-					aux=array[i]; // Intercambiar.
-					array[i]=array[d];
-					array[d]=aux;
-				}
-				else // si se han cruzado:
-					break; // salir del bucle.
-	}
-	if(d==desde-1) // Si la segunda búsqueda se sale del array
-		d=desde; // es que el pivote es el elemento
-
-	// más pequeño: se cambia con él mismo.
-	aux=array[d]; // Colocar el pivote
-	array[d]=array[desde]; // en su posición.
-	array[desde]=aux;
-
-	ordenar(array,desde,d-1); // Ordenar el primer array.
-	ordenar(array,d+1,hasta); // Ordenar el segundo array.
-}
-
-void imprime_uso (){
-    printf("Usage:\n");
-    printf("	qsort -h\n"
+void imprime_uso (FILE *output){
+    fprintf(output, "Usage:\n");
+    fprintf(output, "	qsort -h\n"
            "	qsort -V\n"
            "	qsort [options] archivo\n"
            "Options:\n"
-    	   "	-h, --help		Imprime ayuda.\n"
+    	   "	-h, --help	Imprime ayuda.\n"
     	   "	-V, --version   Version del programa.\n"
     	   "	-o, --output    Archivo de salida.\n"
     	   "	-n, --numeric   Ordenar los datos numericamente en vez de alfabeticamente.\n"
            "Examples:\n"
-    	   "	qsort -n numeros.txt\n");
+    	   "	qsort -n -o salida.txt numeros.txt\n");
 }
 
 void imprime_version(){
@@ -63,10 +30,16 @@ int main(int argc, char* argv[]){
 
 	int siguiente_opcion = 0;
 
-	int numeric = 0; //Por defecto ordena alfabeticamente
+	int numeric = 0; /*Por defecto ordena alfabeticamente*/
+
+	int i, j;
+
+	FILE* fdInput, *fdOutput;
+
+	char* file = NULL; /*Nombre del fichero de entrada*/
 
 	/* El nombre del fichero que recibe la salida del programa */
-	const char* fichero_salida = NULL ;
+	char* fichero_salida = NULL ;
 
 	/* Una cadena que lista las opciones cortas validas */
 	const char* const op_cortas = "hno:V" ;
@@ -89,7 +62,7 @@ int main(int argc, char* argv[]){
 		switch (siguiente_opcion)
 		{
 			case 'h' : /* -h o --help */
-				imprime_uso();
+				imprime_uso(stdout);
 				exit(EXIT_SUCCESS);
 
 			case 'V' : /* -V o --version*/
@@ -105,7 +78,7 @@ int main(int argc, char* argv[]){
 				break;
 
 			case '?' : /* opcion no valida */
-				imprime_uso(); /* codigo de salida 1 */
+				imprime_uso(stderr); /* codigo de salida 1 */
 				exit(1);
 
 			case -1 : /* No hay mas opciones */
@@ -116,65 +89,83 @@ int main(int argc, char* argv[]){
 			}
 	}
 
-	char* file = NULL;
 	file = argv[optind];
 
-	FILE* fd;
-
 	if (file != NULL){
-		fd = fopen(file, "r");
+		fdInput = fopen(file, "r");
 	}
 	else{
-		imprime_uso();
+		fprintf(stderr, "Error: No se especifico el archivo de entrada.\n");
+		imprime_uso(stderr);
 		exit(1);
 	}
 
-	//pude abrir el archivo, estoy situado en la primer linea
-	int lineas= 0;
-	char buffer;
-
-	while (!feof(fd)){
-		buffer= (char)fgetc(fd);
-		if (buffer == '\n'){
-			lineas++;
-		}
+	if(fichero_salida == NULL)
+	{
+		fprintf(stderr, "Error: No se especifico el archivo de salida.\n");
+		imprime_uso(stderr);
+		return -1;
 	}
 
+	/*pude abrir el archivo, estoy situado en la primer linea*/
+	int lineas= 0;
+	char buffer, charAnterior='\0';
+
+	/*Cuento la cantidad de lineas que tiene el archivo*/
+	while (!feof(fdInput)){
+		buffer= (char)fgetc(fdInput);
+		if (buffer == '\n' && charAnterior !='\n'){
+			lineas++;
+		}
+		charAnterior = buffer;
+	}
+
+	/*Recorro de a una las lineas del archivo y las copio en un vector*/
 	char** vector = (char**)malloc(sizeof(char*)*lineas);
-
-	fseek(fd, 0, SEEK_SET);
-
-	int i, j;
-	//recorro de a una las lineas del archivo
+	fseek(fdInput, 0, SEEK_SET);
 	for(i=0; i<lineas; i++)
 	{
 		vector[i] = (char*)malloc(sizeof(char)*MAX);
-		buffer= (char)fgetc(fd);
+		buffer= (char)fgetc(fdInput);
 		j=0;
 		while(buffer != '\n' && j<MAX-1)
 		{
 			vector[i][j] = buffer;
-			buffer= (char)fgetc(fd);
+			buffer= (char)fgetc(fdInput);
 			j++;
 		}
 		vector[i][j] = '\0';
 	}
 
-	printf("Antes de ordenar:\n");
-	for(i=0; i<lineas; i++)
-	{
-		printf("%s\n", vector[i]);
-	}
-
 	quicksort(&vector[0], &vector[lineas-1], numeric);
 
-	printf("\nDespues de ordenar:\n");
+	if(strcmp(fichero_salida, "-") != 0)
+	{
+		fdOutput = fopen(fichero_salida, "wt");
+		if(fdOutput == NULL)
+		{
+			fprintf(stderr, "Error: No se pudo crear el archivo de salida");
+			fclose(fdInput);
+			for(i=0; i<lineas; i++)
+			{
+				free(vector[i]);
+			}
+			free(vector);
+			return -1;
+		}
+	}
+	else
+	{
+		fdOutput = stdout;
+	}
+
 	for(i=0; i<lineas; i++)
 	{
-		printf("%s\n", vector[i]);
+		fprintf(fdOutput, "%s\n", vector[i]);
 		free(vector[i]);
 	}
 	free(vector);
-	fclose(fd);
+	fclose(fdInput);
+	fclose(fdOutput);
 	return 0;
 }
