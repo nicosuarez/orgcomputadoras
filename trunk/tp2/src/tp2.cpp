216dcb158fcffea4238c9682089c6e2025221ba6
mwaisgold <mwaisgold@9c04f0e4-a400-11de-9a17-712dd7b31d32>
2009-11-29 22:52:05 +0000

    Ya no tira mas miss donde no debe.... Pero ahora no tira miss nunca!!! capaz habria que construir el vector con un new. cuando pueda lo veo.

--- trunk/tp2/src/tp2.cpp
+++ trunk/tp2/src/tp2.cpp
@@ -3,18 +3,27 @@
 #include <iostream>
 
 
-void cantidadDeVias(register int vias, register int tamanioCache, register int tamanioBloque){
+void cantidadDeVias(register unsigned int vias, register unsigned int tamanioCache, register unsigned int tamanioBloque){
 	//register char c;
 	//char vector[tamanioCache/ vias][vias];
-	char vector[tamanioCache][vias];
+	register unsigned int i;
+	register unsigned int j;
+	register unsigned int tamanioFor;
+	register unsigned int tamanioVector = tamanioCache/vias;	
+	
+	char vector[tamanioVector][vias];
+	
+//	for (i = 0; i < tamanioVector; i++){
+//		vector[i] = new char[vias];
+//	}
 
-	for(register int j= 0; j< vias; j++){
-		for(register int i= 0; i< tamanioCache/vias/tamanioBloque; i++){
-
+	tamanioFor = tamanioVector / tamanioBloque;
+	for(i= 0; i< tamanioFor; i++){
+		for(j= 0; j< vias; j++){
 			vector[i * tamanioBloque][j]= 'a';
 		}
 	}
-	for(register int j= 0; j< vias; j++){
+	for(j= 0; j< vias; j++){
 		vector[0][j]= 'a';
 	}
 
@@ -23,7 +32,37 @@ void cantidadDeVias(register int vias, register int tamanioCache, register int t
 
 
 int main(int argc, char* argv[]){
-	std::cout << atoi(argv[1]) << " " << atoi(argv[2]) << " " << atoi(argv[3]);
-	cantidadDeVias(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]));
+	std::cout << atoi(argv[1]) << " " << atoi(argv[2]) << " " << atoi(argv[3]) << std::endl;
+	//cantidadDeVias(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]));
+	
+	register unsigned int vias = atoi(argv[1]); 
+	register unsigned int tamanioCache = atoi(argv[2]);
+	register unsigned int tamanioBloque = atoi(argv[3]);
+	register unsigned int i;
+	register unsigned int j;
+	register unsigned int k;
+	register unsigned int tamanioFor;
+		
+	char vector[tamanioCache][vias];
+	
+//	for (i = 0; i < tamanioVector; i++){
+//		vector[i] = new char[vias];
+//	}
+
+	tamanioFor = tamanioCache / tamanioBloque / vias;
+
+	for (k = 0; k < 100; k++){		
+		for(i= 0; i< tamanioFor; i++){
+			for(j= 0; j< vias; j++){
+				vector[i * tamanioBloque][j]= 'a';
+			}
+		}
+	}
+	
+	for(j= 0; j< vias; j++){
+		vector[0][j]= 'a';
+	}
+	
+	
 	return 0;
 }
