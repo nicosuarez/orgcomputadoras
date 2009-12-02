05f85c1edbaeba33a36c68f978436cac9d4b62f7
nicolas.suarez@gmail.com <nicolas.suarez@gmail.com@9c04f0e4-a400-11de-9a17-712dd7b31d32>
2009-12-02 03:06:53 +0000

    Agregue programa principal que deberia correr todos los modulos y hacer los calculos. Por ahora solo funciona para el modulo tamanioBloque

--- trunk/tp2/src/tamanioBloque.cpp
+++ trunk/tp2/src/tamanioBloque.cpp
@@ -1,14 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <iostream>
 
 #define ITERACIONES_BLOQUE 1024 * 1024
 
-void tamanioBloque(){
-	register char c;
-	char* vector = new char[ITERACIONES_BLOQUE];
-	register unsigned int i = 0;
-	for(; i< ITERACIONES_BLOQUE; i++){
+void tamanioBloque(char *vector){
+
+	register unsigned long i = 0;
+	for(i=0; i< ITERACIONES_BLOQUE; i++){
 		vector[i]= 'z';
 	}
 }
+
+int main(int argc, char* argv[]){
+	char vector[ITERACIONES_BLOQUE];
+	tamanioBloque(vector);
+	return 0;
+}
+
